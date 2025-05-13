34b37ccee8dd2a2db9e39ce50ad5c9a96b966c3e
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-13 13:05:41 +0200

    Add logic for fetching user info, display user info on form and for handle submit

--- src/components/forms/EditUserInfoForm.jsx
+++ src/components/forms/EditUserInfoForm.jsx
@@ -1,7 +1,54 @@
+'use client'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
 
 export default function EditUserInfoForm() {
+    const [form, setForm] = useState({
+        image_url: '',
+        github: '',
+        url: '',
+        // group_id: '',
+    })
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        axios
+            .get('/api/user')
+            .then(res => {
+                setForm({
+                    image_url: res.data.data.image_url || '',
+                    github: res.data.data.github || '',
+                    url: res.data.data.url || '',
+                    //   group_id:  res.data.data.group_id?.toString() || '',
+                })
+            })
+            .catch(console.error)
+            .finally(() => setLoading(false))
+    }, [])
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        setForm(f => ({ ...f, [name]: value }))
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        try {
+            const res = await axios.patch('/api/user/profile', form, {
+                headers: { 'Content-Type': 'application/json' },
+            })
+            console.log('Uppdaterad user:', res.data.data)
+            // Show success message
+        } catch (err) {
+            console.error('Valideringsfel eller serverfel', err.response.data)
+            // Show error message
+        }
+    }
+
+    if (loading) return <p>Loading…</p>
+
     return (
         <form className="flex flex-col gap-4">
             <h2 className="text-2xl font-bold">Edit User Info</h2>
