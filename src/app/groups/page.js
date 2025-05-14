4fffce3c2f028db34665b4b342e95833627a7bb4
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-05-14 11:34:37 +0200

    Add AmusementsInfo component to /groups

--- src/app/groups/page.js
+++ src/app/groups/page.js
@@ -1,8 +1,10 @@
 import GroupData from '@/components/group-information/GroupInfo'
+import AmusementsInfo from '@/components/amusements-information/AmusementsInfo'
 
 export default function Groups() {
     return (
         <div>
+            <AmusementsInfo />
             <GroupData />
         </div>
     )
