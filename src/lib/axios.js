335136d8223aa59f4ed267bf73d0dd4f5c863d78
Andreawingardh <andreawingardh@gmail.com>
2025-05-15 13:39:29 +0200

    deploy fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,8 +5,24 @@ const axios = Axios.create({
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
-    withCredentials: true,
-    withXSRFToken: true
+    withCredentials: true,  // Send cookies with each request (important for CSRF)
+})
+
+// Add a request interceptor to include the CSRF token automatically
+axios.interceptors.request.use((config) => {
+    // Get CSRF token from cookies (make sure it exists)
+    const xsrfToken = document.cookie
+        .split(';')
+        .find((cookie) => cookie.trim().startsWith('XSRF-TOKEN='))
+        ?.split('=')[1]
+
+    if (xsrfToken) {
+        config.headers['X-XSRF-TOKEN'] = xsrfToken  // Send the token with the request
+    }
+
+    return config
+}, (error) => {
+    return Promise.reject(error)
 })
 
 export default axios
