9c5142558fa4542ef96803e2c148d1e46a0d1378
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-19 15:23:56 +0200

    Add ProfileCard component

---
+++ src/components/group-information/ProfileCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function ProfileCard({ user }) {
+    return (
+        <div className="flex items-center bg-gray-100 p-4 rounded-lg space-x-4">
+            {/* Profileimage-placeholder */}
+            <div className="w-16 h-16 bg-white rounded-full flex-shrink-0" />
+
+            {/* Textcontainer */}
+            <div>
+                {/* Namme */}
+                <p className="text-lg font-medium text-black">{user.name}</p>
+
+                {/* Links */}
+                <div className="mt-1 flex items-center text-sm text-gray-600 space-x-2">
+                    {user.github && (
+                        <a
+                            href={user.github}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:underline">
+                            GITHUB
+                        </a>
+                    )}
+
+                    {/* Show ”|” only if both website and github links are provided */}
+                    {user.github && user.website && <span>|</span>}
+
+                    {user.website && (
+                        <a
+                            href={user.website}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:underline">
+                            WEBSITE
+                        </a>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
