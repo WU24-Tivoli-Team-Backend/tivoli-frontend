e31904dd4ae11ff7224b711c0f80627d5071b891
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-22 11:37:41 +0200

    Remove header from dashboard, change max width

--- src/app/(app)/dashboard/page.js
+++ src/app/(app)/dashboard/page.js
@@ -1,4 +1,4 @@
-import Header from '@/app/(app)/Header'
+//import Header from '@/app/(app)/Header'
 import UserProfile from '@/components/profile-information/UserProfile'
 import UserAmusementContainer from '@/components/amusement/UserAmusementContainer'
 
@@ -11,10 +11,10 @@ export const metadata = {
 const Dashboard = () => {
     return (
         <>
-            <Header title="Dashboard" />
+            {/* <Header title="Dashboard" /> */}
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-col gap-8">
                         <div className="p-6 bg-white border-b border-gray-200">
                             You are logged in!
                         </div>
