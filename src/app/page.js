735eb523f6ede2712bd518f3cc8fe33030f7cf98
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-22 15:09:02 +0200

    Improve styling on frontpage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import LoginLinks from '@/app/LoginLinks'
 import Image from 'next/image'
-// import GuestLoginButton from '@/components/GuestLoginButton'
+import GuestLoginButton from '@/components/GuestLoginButton'
 
 export const metadata = {
     title: 'YRGO TIVOLI',
@@ -9,13 +9,21 @@ export const metadata = {
 const Home = () => {
     return (
         <>
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
                 <LoginLinks />
 
-                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 flex flex-col content-center justify-center">
-                    <h1 className="text-8xl text-center">WELCOME TO YRGO TIVOLI</h1>
-                    <Image src="/red-panda-front-page.png" alt='red panda' width={500} height={500} />
-                    {/*This will be added after May 26th: <GuestLoginButton className="size-fit"/> */}
+                <div className="flex flex-col items-center gap-8 pt-16 sm:pt-48 p-6">
+                    <h1 className="text-8xl text-center">YRGO TIVOLI</h1>
+                    <Image
+                        src="/images/red-panda-wide.png"
+                        alt="red panda"
+                        width={500}
+                        height={500}
+                        priority // Preload the image for better performance
+                        style={{ width: 'auto', height: 'auto' }} // Ensure aspect ratio is maintained
+                    />
+                    {/* This will be added after May 26th:{' '} */}
+                    <GuestLoginButton className="size-fit" />
                 </div>
             </div>
         </>
