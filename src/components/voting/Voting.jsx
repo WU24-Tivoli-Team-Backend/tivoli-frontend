b83b89e6e82d8fd7b51cf6215da4c5ac3166e9f3
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-23 16:59:13 +0200

    Add white background to voting page (#172)

--- src/components/voting/Voting.jsx
+++ src/components/voting/Voting.jsx
@@ -79,7 +79,7 @@ export default function Voting() {
         return <p>Error: {(voteError || amusementError).message}</p>
 
     return (
-        <div className="max-w-2xl mx-auto p-4">
+        <div className="max-w-2xl mx-auto p-4 bg-white rounded shadow-md">
             <h1 className="text-2xl font-bold mb-4">
                 Vote for Your Favorite Amusement
             </h1>
