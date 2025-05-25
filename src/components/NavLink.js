7efa2018bc8b1440e759b9985776260321acfc4c
PU-MEriksson <malin.eriksson83@icloud.com>
2025-05-25 14:52:31 +0200

    Change text color to white

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 
-const NavLink = ({ active = false, children, ...props }) => (
+const NavLink = ({
+    active = false,
+    children,
+    className = '',
+    activeClassName = '',
+    ...props
+}) => (
     <Link
         {...props}
-        className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out ${
+        className={`inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-300 ease-in-out border-b-2 ${
             active
-                ? 'border-indigo-400 text-gray-900 focus:border-indigo-700'
-                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'
+                ? `text-white border-white font-bold ${activeClassName}`
+                : `border-transparent text-white/90 hover:text-white hover:scale-105 ${className}`
         }`}>
         {children}
     </Link>
